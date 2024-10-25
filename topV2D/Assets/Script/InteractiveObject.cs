c133bac64ad97bb8472337e3d0c9268fa9a2cea0
geoni-ara <145415923+geoni-ara@users.noreply.github.com>
2024-10-25 23:02:57 +0900

    물체 상호작용

---
+++ topV2D/Assets/Script/InteractiveObject.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class InteractiveObject : MonoBehaviour
+{
+
+    public GameObject AttachObject;
+    public Transform AttachPoint;
+    bool isHolding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(isHolding){
+                DropObject();
+            }else{
+                CheckObject();
+                if(AttachObject != null){
+                    PickUpObject();
+                }
+            }
+        }
+    }
+
+    void CheckObject(){
+        float radius = 0.9f;
+        Vector3 position = transform.position;
+
+        Collider[] hitColliders = Physics.OverlapSphere(position, radius);
+
+        AttachObject = null;
+
+        foreach(Collider hitCollider in hitColliders){
+            if(hitCollider.gameObject.layer == 6){
+                AttachObject = hitCollider.gameObject;
+                break;
+            }
+        }
+    }
+
+    void PickUpObject(){
+        if(AttachObject != null){
+            AttachObject.transform.position = AttachPoint.position;
+            AttachObject.transform.rotation = AttachPoint.rotation;
+
+            AttachObject.transform.SetParent(AttachPoint);
+            isHolding = true;   
+        }
+    }
+
+    void DropObject(){
+        if(AttachObject != null){
+            AttachObject.transform.SetParent(null);
+
+            AttachObject = null;
+            isHolding = false;
+        }
+    }
+}
